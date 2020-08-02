91997249612366118befbb5084d94fa240dcad35
sushrutnaik2018 <sushrut.naik@nitorinfotech.com>
2020-08-02 14:12:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zjkabm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zjkabm)
---
+++ src/app/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    name: 'Google Pixel XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    tooltipdetails: '13 MP Camera, Octa Core Processor,64 GB Ram,Andriod Pie',
+    features: '13 MP Camera, Octa Core Processor,64 GB Ram,Andriod Pie'
+  },
+  {
+    name: 'Google Pixel Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    tooltipdetails: '13 MP Camera, Octa Core Processor,32 GB Ram,Andriod Nugget',
+    features: '13 MP Camera, Octa Core Processor,32 GB Ram,Andriod Nugget'
+  },
+  {
+    name: 'Google Nexus',
+    price: 299,
+    description: '',
+    tooltipdetails: '10 MP Camera, Quad Core Processor,16 GB Ram,Andriod Marshmellow',
+    features: '10 MP Camera, Quad Core Processor,16 GB Ram,Andriod Marshmellow'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
